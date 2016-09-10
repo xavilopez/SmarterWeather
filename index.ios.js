b244edf032f4a2c503896e26d86251572500754f
xavilopez <xavi_lopez011@hotmail.com>
2016-09-10 12:44:13 -0400

    Added SmarterWeather
    Added SmarterWeather

---
+++ index.ios.js
@@ -0,0 +1,10 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import {AppRegistry} from 'react-native';
+import SmarterWeather from './weather_project';
+
+AppRegistry.registerComponent('SmarterWeather', () => SmarterWeather);
